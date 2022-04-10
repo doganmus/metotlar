cfc2c41b85e9d0f86894294636aadb6234978ed2
Dogan Mus <doganmus87@gmail.com>
2022-04-11 00:15:50 +0300

    pat

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //erisim_belirteci geri_donustipi metot_adi(parametreListesi/arguman)
+            //{
+                 //komutlar;
+                 //return;   
+            //}
+            int a = 2;
+            int b = 3;
+            Console.WriteLine(a+b);
+
+            int sonuc =  Topla(a,b);
+
+            Console.WriteLine(sonuc);
+
+            Metotlar ornek = new Metotlar();
+            ornek.EkranaYazdır(Convert.ToString(sonuc));
+
+            
+            int sonuc2 =  ornek.ArttirVeTopla(ref a,ref b);
+            ornek.EkranaYazdır(Convert.ToString(sonuc2));
+            ornek.EkranaYazdır(Convert.ToString(a+b));
+        }
+        static int Topla(int deger1, int deger2)
+        {
+            return (deger1+deger2);
+        }
+    }
+
+    class Metotlar
+    {
+        public void EkranaYazdır(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+
+        public int ArttirVeTopla(ref int deger1, ref int deger2)
+        {
+            deger1+=1;
+            deger2+=1;
+            return deger1+deger2;
+
+        }
+    }
+}
